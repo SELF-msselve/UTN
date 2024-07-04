60e030448c619cdc52a174e0d71b800ffba53265
FARINA SAINT SELVE <RY09678@grupo.ypf.com>
2024-07-04 19:03:03 -0300

    1

--- Test/BirdApp.py
+++ Test/BirdApp.py
@@ -1,46 +1,69 @@
+import pandas as pd
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.actionbar import ActionBar, ActionPrevious
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
-from kivy.uix.image import Image
 from kivy.core.window import Window
+from kivy.uix.image import Image
 from kivy.clock import Clock
-#from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import Rectangle
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
-import pandas as pd
 from pipe import Pipe
-
-#df is your dataframe
-#example function is applicable for all INT dataframe 
-
 from random import randint
 from kivy.properties import NumericProperty
 
-class UnificaScreen():
+global df_global
+dataNombre = ['Lucas','Stella','Eduardo']
+dataScore = [0,0,0]
+data = {'Nombre': dataNombre, 'Score': dataScore}
+df_global = pd.DataFrame(data)
+        
+class Unifica_Screen(Screen):
+    #Se generan las variables globales del juego
     pipes = []
     GRAVITY = 300
     was_colliding = False
     pipe_diviation = 100
-    
-    dataOrden = [1,2,3,4,5,6,7,8,9,10]
-    dataNombre = ['Lucas','Stella','Eduardo','Anto','Joyi', 'Yayo','Lucy','Alberto', 'Martin', 'XXXXX']
-    dataScore = [1,2,3,4,5,6,7,8,9,10]
-    data = {'Orden': dataOrden, 'Nombre': dataNombre, 'Score': dataScore}
-    df_score = pd.DataFrame(data)
+    entered_text = 'Nombre'
+
+    #Esta funcion genera la lista de los Scores
+    def genera_listas(self, header, table, lista):
+        #Se limpia la lista para generarla nuevamente con el nuevo score
+        self.ids[header].clear_widgets()
+        self.ids[table].clear_widgets()
+        
+        self.ids[header].cols = len(lista)
+        self.ids[table].cols = len(lista)
+        for column_name in lista:
+            self.ids[header].add_widget(Label(text=str(column_name), bold=True, size_hint_y=None, height=40))
+
+        for index, row in df_global[lista].iterrows():
+            for cell in row:
+                if index % 2 == 0:
+                    self.ids[table].add_widget(Label(text=str(cell), size_hint_y=None, height=40, color=(0,1,1,1)))
+                else:
+                    self.ids[table].add_widget(Label(text=str(cell), size_hint_y=None, height=40, color=(1,1,1,1)))
 
 class Background(Widget):
+    #Objetos para crear nuebes y el piso.
     cloud_texture = ObjectProperty(None)
     floor_texture = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        # Create textures
+        # Crea las texturas
         self.cloud_texture = Image(source="cloud.png").texture
         self.cloud_texture.wrap = 'repeat'
         self.cloud_texture.uvsize = (Window.width / self.cloud_texture.width, -1)
@@ -48,26 +71,24 @@ class Background(Widget):
         self.floor_texture = Image(source="floor.png").texture
         self.floor_texture.wrap = 'repeat'
         self.floor_texture.uvsize = (Window.width / self.floor_texture.width, -1)
-        
-    
 
     def on_size(self, *args):
         self.cloud_texture.uvsize = (self.width / self.cloud_texture.width, -1)
         self.floor_texture.uvsize = (self.width / self.floor_texture.width, -1)
 
     def scroll_textures(self, time_passed):
-        # Update the uvpos of the texture
+        # Funcion que mueve las texturas para generar idea de avance.
         self.cloud_texture.uvpos = ( (self.cloud_texture.uvpos[0] + time_passed/2.0)%Window.width , self.cloud_texture.uvpos[1])
         self.floor_texture.uvpos = ( (self.floor_texture.uvpos[0] + time_passed)%Window.width, self.floor_texture.uvpos[1])
 
-        # Redraw the texture
+        # Se re dibujan las texturas
         texture = self.property('cloud_texture')
         texture.dispatch(self)
 
         texture = self.property('floor_texture')
         texture.dispatch(self)
 
-
+#Clase que crea el pajaro con sus imagenes de movimiento
 class Bird(Image):
     velocity = NumericProperty(0)
 
@@ -80,29 +101,20 @@ class Bird(Image):
         self.source = "bird1.png"
         super().on_touch_up(touch)
 
+#Ventana de inicio del Juego con LOGO        
+class StartWindow(Unifica_Screen):
+    pass
+#Ventana que para mostrar la tabla de Score.
+class ScoreWindow(Unifica_Screen):
+    def on_enter(self):
+        lista = ['Nombre','Score']  
+        header = 'unidades_header_layout'
+        table = 'unidades_table_layout'
+        Unifica_Screen.genera_listas(self, header, table, lista )
 
-
-
-class ScoreWindow(BoxLayout, UnificaScreen):
-    def __init__(self, **kwargs):
-        super(ScoreWindow, self).__init__(**kwargs)
-        start_button = Button(text="Start Game")
-        start_button.bind(on_press=self.start_game)
-        self.add_widget(start_button)
-        
-    def start_game(self, instance):
-        App.get_running_app().switch_to_GameWindow()
-   
-
-class GameWindow(FloatLayout, UnificaScreen):
-    def __init__(self, **kwargs):
-        super(GameWindow, self).__init__(**kwargs)
-    
-    pipes = []
-    GRAVITY = 300
-    was_colliding = False
-    pipe_diviation = 100
-
+#Ventana principal del juego.
+class GameWindow(Unifica_Screen, FloatLayout):
+#Funciones que asignan los valores para las dificultades de los niveles.
     def pipe_deviation_easy(self):
         self.pipe_diviation = 200
         Pipe.GAP_SIZE = 200
@@ -124,9 +136,6 @@ class GameWindow(FloatLayout, UnificaScreen):
         self.ids.lvl_2.disabled = False
         self.ids.lvl_3.disabled = True
 
-    #def on_start(self):
-    #    Clock.schedule_interval(self.root.ids.background.scroll_textures, 1/60.)
-
     def move_bird(self, time_passed):
         bird = self.ids.bird
         bird.y = bird.y + bird.velocity * time_passed
@@ -135,7 +144,7 @@ class GameWindow(FloatLayout, UnificaScreen):
 
     def check_collision(self):
         bird = self.ids.bird
-        # Go through each pipe and check if it collides
+        # Verifica las coliciones del pajaro
         is_colliding = False
         for pipe in self.pipes:
             if pipe.collide_widget(bird):
@@ -149,38 +158,37 @@ class GameWindow(FloatLayout, UnificaScreen):
             self.game_over()
         if bird.top > Window.height:
             self.game_over()
-
+        #Si no existe colicion se suma un punto al Score
         if self.was_colliding and not is_colliding:
             self.ids.score.text = str(int(self.ids.score.text)+1)
         self.was_colliding = is_colliding
 
-  
+#Funcion que se llama al existir una colicion.
+#Levanta un popup para que el jugador cargue su nombre y registre su score.  
     def game_over(self):
+        
         # Crear una ventana emergente (popup)
-        popup = Popup(title="Ventana Secundaria", size_hint=(0.8, 0.6))
+        self.popup = Popup(title="Ventana Secundaria", size_hint=(0.8, 0.6))
         
         # Crear un layout para la ventana secundaria
-        secondary_layout = BoxLayout(orientation="vertical")
+        self.secondary_layout = BoxLayout(orientation="vertical")
         
         # Crear un campo de texto para ingresar texto
-        text_input = TextInput(hint_text="Ingresa tu texto aquí")
-        secondary_layout.add_widget(text_input)
+        self.text_input = TextInput(hint_text=self.entered_text)
+        self.secondary_layout.add_widget(self.text_input)
         
         # Crear un botón para cerrar la ventana secundaria
         close_button = Button(text="Cerrar Ventana Secundaria")
-        close_button.bind(on_press=popup.dismiss)
-        secondary_layout.add_widget(close_button)
+        close_button.bind(on_press=self.close_popup)
+        self.secondary_layout.add_widget(close_button)
         
         # Agregar el layout a la ventana secundaria
-        popup.content = secondary_layout
+        self.popup.content = self.secondary_layout
         
         # Mostrar la ventana secundaria
-        popup.open()  
+        self.popup.open()
         
-        self.clear_widgets()
-        self.add_widget(ScoreWindow())
-        
-        
+        # Desactivan todo los botones de nivel.         
         self.ids.lvl_1.disabled = False
         self.ids.lvl_2.disabled = False
         self.ids.lvl_3.disabled = False
@@ -191,7 +199,26 @@ class GameWindow(FloatLayout, UnificaScreen):
         self.ids.start_button.disabled = False
         self.ids.start_button.opacity = 1
 
-
+    #Funcion que se llama al cerrar el popup y actualiza el df de Score.
+    #Se agrega el nuevo jugador y se ordena la tabla de mayor a menor.
+    def close_popup(self, instance):
+        # Capturar el valor del texto ingresado
+        entered_text = self.text_input.text
+        print(f'Text entered: {entered_text}')
+        print(self.ids.score.text)
+        global df_global
+        data2 = {'Nombre': [entered_text], 'Score': [int(self.ids.score.text)]}
+        df = pd.DataFrame(data2)
+        df_global = pd.concat([df_global, df], ignore_index=True)
+        df_global.sort_values(by='Score', ascending=False, inplace=True)
+        
+        app = App.get_running_app()
+        app.cambia_ScoreWindow() 
+        
+        # Cerrar el popup
+        self.popup.dismiss()
+    
+    
     def next_frame(self, time_passed):
         self.move_bird(time_passed)
         self.move_pipes(time_passed)
@@ -204,10 +231,9 @@ class GameWindow(FloatLayout, UnificaScreen):
         self.ids.score.text = "0"
         self.was_colliding = False
         self.pipes = []
-        #Clock.schedule_interval(self.move_bird, 1/60.)
         self.frames = Clock.schedule_interval(self.next_frame, 1/60.)
 
-        # Create the pipes
+        # Crea los pipes
         num_pipes = 5
         distance_between_pipes = Window.width / (num_pipes - 1)
         for i in range(num_pipes):
@@ -220,15 +246,12 @@ class GameWindow(FloatLayout, UnificaScreen):
             self.pipes.append(pipe)
             self.add_widget(pipe)
 
-        # Move the pipes
-        #Clock.schedule_interval(self.move_pipes, 1/60.)
-
     def move_pipes(self, time_passed):
-        # Move pipes
+        # Mueve los pipes
         for pipe in self.pipes:
             pipe.x -= time_passed * 100
 
-        # Check if we need to reposition the pipe at the right side
+        # Verifica de ir agregando nuevos pipes a la ventana.
         num_pipes = 5
         distance_between_pipes = Window.width / (num_pipes - 1)
         pipe_xs = list(map(lambda pipe: pipe.x, self.pipes))
@@ -236,20 +259,20 @@ class GameWindow(FloatLayout, UnificaScreen):
         if right_most_x <= Window.width - distance_between_pipes:
             most_left_pipe = self.pipes[pipe_xs.index(min(pipe_xs))]
             most_left_pipe.x = Window.width
-    
 
-class MainApp(App):
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file('BirdApp.kv')
+
+class AwesomeApp(App):
     def build(self):
-        #self.root = GameWindow()
-        self = ScoreWindow()
-        return self
-    
-    def switch_to_GameWindow(self):
-        self.root.clear_widgets()
-        self.root.add_widget(GameWindow())
-    
+        return kv
+        
+    def cambia_ScoreWindow(self):
+        self.root.ids.screen_manager.current = 'ScoreWindow'
 
 
-
-if __name__ == "__main__":
-    MainApp().run()
+if __name__ == '__main__':
+    AwesomeApp().run() 
